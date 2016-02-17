1792f972db18813b839b636342b63292c8882ca7
Scott <starkey9000@gmail.com>
2016-02-16 21:26:33 -0500

    Commit after removing venv
    Removed the virtual environment from commit

---
+++ alien_invasion.py
@@ -0,0 +1,33 @@
+import pygame
+from settings import Settings
+from ship import Ship
+#from alien import Alien
+import game_functions as gf
+from pygame.sprite import Group
+
+def run_game():
+    #Initialize pygame, settings and screen object
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #Make a ship, a group of bullets, and a group of aliens
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    #Create the fleet of aliens
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    
+
+    #Start the main loop of the game
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, ship, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+
+run_game()
