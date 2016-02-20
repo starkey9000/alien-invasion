d19c22d895e509910d965b993aa61da9fd139188
Scott <starkey9000@gmail.com>
2016-02-19 22:13:07 -0500

    play can be pushed more than once
    Play button would only work one time - fixed now can be pushed multiple
    times

--- game_functions.py
+++ game_functions.py
@@ -4,7 +4,7 @@ import pygame
 from bullet import Bullet
 from alien import Alien
 
-def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     #Watch for keyboard and mouse events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -13,7 +13,7 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats, play_button, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
             
         elif event.type == pygame.KEYDOWN:
             check_keydown_events(event, ai_settings, screen, ship, bullets)
@@ -21,10 +21,20 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     '''Start a new game when the player clicks play'''
     if play_button.rect.collidepoint(mouse_x, mouse_y):
+        #Reset the game statistics
+        stats.reset_stats()
         stats.game_active = True
+
+        #Empty the list of aliens and bullets
+        aliens.empty()
+        bullets.empty()
+
+        #Create a new fleet and center the ship
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
             
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
